4e9847a1063d5a7e4f253f83dda6b4ab26556783
刘lyxAndy <65491750+liulyxandy-codemao@users.noreply.github.com>
2022-11-11 21:02:56 +0800

    Update main.autoload.js

--- main.autoload.js
+++ main.autoload.js
@@ -13,33 +13,35 @@ document.getElementsByTagName("body")[0].appendChild(z)
 //window.VAPTCHABotCheck.data = null;
 // Vaptcha UI
 var i=setInterval(function(){
-    if(vaptcha){clearInterval(i)}
+    if(vaptcha){
+        vaptcha({
+            vid: '636e1f68cdf7d074d80a484d',
+            mode: 'click',
+            scene: 1,
+            container: '#vaptcha',
+            area: 'auto',
+        }).then(function (VAPTCHAObj) {
+            // 将VAPTCHA验证实例保存到局部变量中
+            obj = VAPTCHAObj;
+            
+            // 渲染验证组件
+            VAPTCHAObj.render();
+
+            // 验证成功进行后续操作
+            VAPTCHAObj.listen('pass', function () {
+                serverToken = VAPTCHAObj.getServerToken();
+                var data = {
+                    server: serverToken.server,
+                    token: serverToken.token,
+                }
+                //window.VAPTCHABotCheck.status = true;
+                //window.VAPTCHABotCheck.data = data;
+                window.postMessage(data)
+            })
+        })
+        clearInterval(i)}
     console.log(1)
 },1000)
 
 
-vaptcha({
-    vid: '636e1f68cdf7d074d80a484d',
-    mode: 'click',
-    scene: 1,
-    container: '#vaptcha',
-    area: 'auto',
-}).then(function (VAPTCHAObj) {
-    // 将VAPTCHA验证实例保存到局部变量中
-    obj = VAPTCHAObj;
-    
-    // 渲染验证组件
-    VAPTCHAObj.render();
 
-    // 验证成功进行后续操作
-    VAPTCHAObj.listen('pass', function () {
-        serverToken = VAPTCHAObj.getServerToken();
-        var data = {
-            server: serverToken.server,
-            token: serverToken.token,
-        }
-        //window.VAPTCHABotCheck.status = true;
-        //window.VAPTCHABotCheck.data = data;
-        window.postMessage(data)
-    })
-})
